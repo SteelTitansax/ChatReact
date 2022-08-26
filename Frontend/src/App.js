5e6558116fc4364cd23a673bf45eb50a80dc0eff
SteelTitansax <manuel.portero.leiva@gmail.com>
2022-08-26 19:05:48 +0200

    second commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import {BrowserRouter as Router, Route } from 'react-router-dom'; 
+import Join from './components/Join/Join.js'; 
+import Chat from './components/Chat/Chat.js'; 
+
+const App = () => (
+<Router>
+    <Route path="/" exact component={Join} />
+    <Route path="/chat" exact component={Chat} />
+</Router>
+
+);
+
+export default App; 
